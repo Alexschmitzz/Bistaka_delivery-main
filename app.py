3df204d344793c01dcd3c7f48525b428d99f93db
Alexschmitzz <alex.schimitz29@gmail.com>
2026-03-01 22:53:07 -0300

    debug off

--- app.py
+++ app.py
@@ -425,4 +425,4 @@ def lista_clientes():
 
 if __name__ == '__main__':
     # Roda acessível na rede local
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)

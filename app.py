56e7a0eb43b8180840162325aab2013726dd0cae
Alexschmitzz <alex.schimitz29@gmail.com>
2026-04-06 01:34:19 -0300

    att bd

--- app.py
+++ app.py
@@ -462,4 +462,4 @@ def lista_clientes():
     return render_template('clientes.html', clientes=clientes)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=4000, debug=False)
